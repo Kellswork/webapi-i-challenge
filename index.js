ceb660816d4720d9924509fb595d84a532f89ae3
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-07-08 14:18:15 +0100

    Implement get users and get user by id endpoints

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+
+const express = require('express');
+const db = require('./data/db');
+const server = express();
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: 'The users information could not be retrieved.' });
+    });
+});
+
+server.get('/api/users/:id', (req, res) => {
+  db.findById(req.params.id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
+    })
+    .catch(() => {
+      res
+        .status(500)
+        .json({ error: 'The users information could not be retrieved.' });
+    });
+});
+
+server.listen(4500, () => {
+  console.log('server listening on port 4500');
+});
